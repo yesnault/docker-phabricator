d2fbbab815ca1723070952b5da68482ad0c22e81
Martin Mouterde <martin.mouterde@zenika.com>
2014-08-14 02:32:20 +0200

    Update run-server.sh
    change the image name

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -i -d -p 3306:3306 -p 8081:80 -p 2244:22 -v /data/phabricator/mysql:/var/lib/mysql -v /data/phabricator/repo:/var/repo -v /data/phabricator/conf:/opt/phabricator/conf yesnault/phabricator 
+docker run -i -d -p 3306:3306 -p 8081:80 -p 2244:22 -v /data/phabricator/mysql:/var/lib/mysql -v /data/phabricator/repo:/var/repo -v /data/phabricator/conf:/opt/phabricator/conf yesnault/docker-phabricator
 
